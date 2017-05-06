84d54b23abdbe0cd8dd21743c2ed4fe06bac1a46
Hassam Ali <hassam.ali7@hotmail.com>
2017-05-06 18:40:06 +0500

    Increase speed of bullet

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ class Settings():
         self.screen_height = 600
         self.bg_color = (230, 230, 230)
         self.ship_speed_factor = 1.5
-        self.bullet_speed_factor = 1
+        self.bullet_speed_factor = 3
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 0, 0, 0
