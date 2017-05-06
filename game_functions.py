e20b789b13ba0c3862af2cc2f3d8ecbcffcd3ab6
Hassam Ali <hassam.ali7@hotmail.com>
2017-05-07 00:52:35 +0500

    Pause Game if all lives are used

--- game_functions.py
+++ game_functions.py
@@ -106,12 +106,15 @@ def update_aliens(ai_settings,stats,screen,ship,aliens,bullets):
     check_aliens_bottom(ai_settings,stats,screen,ship,aliens,bullets)
         
 def ship_hit(ai_settings,stats,screen,ship,aliens,bullets):
-    stats.ships_left -= 1
-    aliens.empty()
-    bullets.empty()
-    create_fleet(ai_settings,screen,ship,aliens)
-    ship.center_ship()
-    sleep(0.5)
+    if stats.ships_left > 0:
+        stats.ships_left -= 1
+        aliens.empty()
+        bullets.empty()
+        create_fleet(ai_settings,screen,ship,aliens)
+        ship.center_ship()
+        sleep(0.5)
+    else:
+        stats.game_active = False
 
 def check_aliens_bottom(ai_settings,stats,screen,ship,aliens,bullets):
     screen_rect = screen.get_rect()
