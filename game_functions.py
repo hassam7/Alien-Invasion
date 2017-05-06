6062cc537a494644f9e2cbf09c9d87cae03c5ea9
Hassam Ali <hassam.ali7@hotmail.com>
2017-05-07 02:08:48 +0500

    Hide or Display cursor

--- game_functions.py
+++ game_functions.py
@@ -83,6 +83,7 @@ def check_events(ai_settings,screen,stats,play_button,ship,aliens,bullets):
 def check_play_button(ai_settings,screen,stats,play_button,ship,aliens,bullets,mouse_x,mouse_y):
     button_clicked = play_button.rect.collidepoint(mouse_x,mouse_y)
     if button_clicked and not stats.game_active:
+        pygame.mouse.set_visible(False)
         stats.reset_stats()
         stats.game_active = True
         aliens.empty()
@@ -130,7 +131,8 @@ def ship_hit(ai_settings,stats,screen,ship,aliens,bullets):
         sleep(0.5)
     else:
         stats.game_active = False
-
+        pygame.mouse.set_visible(True)
+        
 def check_aliens_bottom(ai_settings,stats,screen,ship,aliens,bullets):
     screen_rect = screen.get_rect()
     for alien in aliens.sprites():
