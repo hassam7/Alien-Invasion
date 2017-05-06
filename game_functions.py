c82786a86772f376e00842072645ec84b5c82c65
Hassam Ali <hassam.ali7@hotmail.com>
2017-05-06 18:49:49 +0500

    refactored update_bullets

--- game_functions.py
+++ game_functions.py
@@ -48,6 +48,11 @@ def fire_bullet(ai_settings,screen,ship,bullets):
             new_bullet = Bullet(ai_settings, screen, ship)
             bullets.add(new_bullet)
 
+def check_bullet_alien_collisions(ai_settings,screen,ship,aliens,bullets):
+    collisions = pygame.sprite.groupcollide(bullets,aliens,True,True)
+    if len(aliens) == 0:
+        bullets.empty()
+        create_fleet(ai_settings,screen,ship,aliens)
 
 
 def update_bullets(ai_settings,screen,ship,aliens,bullets):
@@ -55,11 +60,8 @@ def update_bullets(ai_settings,screen,ship,aliens,bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0 :
             bullets.remove(bullet)
-    collisions = pygame.sprite.groupcollide(bullets,aliens,True,True)
-    if len(aliens) == 0:
-        bullets.empty()
-        create_fleet(ai_settings,screen,ship,aliens)
-
+    check_bullet_alien_collisions(ai_settings,screen,ship,aliens,bullets)
+    
 def check_keyup_events(event, ship):
     if event.key == pygame.K_RIGHT:
         ship.moving_right = False
