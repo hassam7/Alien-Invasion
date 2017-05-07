e509c57e2576a7649283f25f0606b13347414fdf
Hassam7 <hassam.ali7@hotmail.com>
2017-05-07 23:43:07 +0500

    speed up after all aliens are killed

--- game_functions.py
+++ game_functions.py
@@ -52,6 +52,7 @@ def check_bullet_alien_collisions(ai_settings,screen,ship,aliens,bullets):
     collisions = pygame.sprite.groupcollide(bullets,aliens,True,True)
     if len(aliens) == 0:
         bullets.empty()
+        ai_settings.increase_speed()
         create_fleet(ai_settings,screen,ship,aliens)
 
 
