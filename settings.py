c6fc099aacbda4879dafdbdff2c6a4c23ac496c0
Hassam7 <hassam.ali7@hotmail.com>
2017-05-07 23:37:20 +0500

    added dynamic_settings to settings.py

--- settings.py
+++ settings.py
@@ -3,13 +3,19 @@ class Settings():
         self.screen_width = 1200
         self.screen_height = 600
         self.bg_color = (230, 230, 230)
-        self.ship_speed_factor = 1.5
-        self.bullet_speed_factor = 3
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 0, 0, 0
         self.bullets_allowed = 3
-        self.alien_speed_factor = 1
         self.fleet_drop_speed = 10
-        self.fleet_direction = 1
         self.ship_limit = 3        
+        self.speed_scale = 1.1
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
+        self.alien_speed_factor = 1
+        self.fleet_direction = 1
+
+    
